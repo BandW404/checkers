a5ff02c92dfb9d0384ce3aade1117b6354f7e205
FoKycHuK-book <parasha@gmail.com>
2014-04-09 14:13:24 +0600

    init tests

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace checkers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void MustAttack()
+        {
+            //var validator = new Validator();
+            var field = new Checker[8, 8];
+            field[2, 2] = new Checker(Color.White, false);
+            field[3, 3] = new Checker(Color.Black, false);
+            //var moveInfo = new MoveInfo(new Point(2, 2), new Point(
+            //validator.IsCorrectMove(MoveInfo moveInfo, Color playerColor)
+        }
+    }
+}
