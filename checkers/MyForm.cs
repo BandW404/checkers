e53dc1c515ba805db6cdcd7bb9158285cfc9c5b1
FoKycHuK-book <parasha@gmail.com>
2014-04-10 13:52:14 +0600

    задатки формочки

---
+++ checkers/MyForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace checkers
+{
+    public class MyForm : Form
+    {
+        Timer timer;
+        const int ElementSize = 64;
+        Bitmap bitmapBlack;
+        Bitmap bitmapWhite;
+        int tickCount;
+
+        public MyForm()
+        {
+            StartPosition = FormStartPosition.CenterScreen;
+            ClientSize = new Size(ElementSize * 8, ElementSize * 8);
+        }
+    }
+}
