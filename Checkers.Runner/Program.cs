a624b6b89aaa0a7e4a866fee26b9a672b8852061
FoKycHuK-book <parasha@gmail.com>
2014-04-27 15:21:21 +0600

    как-то не удалось в этот раз

--- Checkers.Runner/Program.cs
+++ Checkers.Runner/Program.cs
@@ -1,32 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 
 namespace Checkers.Runner
 {
     class Program
     {
+        static JavaScriptSerializer serializer = new JavaScriptSerializer();
         static void Main(string[] args)
         {
-
-
-            //var assembly = Assembly.LoadFrom(args[0]);
-
-            //var player = assembly
-            //    .GetTypes()
-            //    .Where(z => z.GetInterfaces().Any(x => x == typeof(IPlayer)))
-            //    .FirstOrDefault();
-
+            var assembly = Assembly.LoadFrom(args[0]);
+            Color color;
+            if (args[1] == "White")
+                color = Color.White;
+            else
+                color = Color.Black;
+            var player = assembly
+                .GetTypes()
+                .Where(z => z.GetInterfaces().Any(x => x.ToString() == typeof(IPlayer).ToString()))
+                .FirstOrDefault();
             //var ctor = player
-            //    .GetConstructor(new Type[] { });
-
-            //var playerObjct = ctor
-            //    .Invoke(new object[] { }) as IPlayer;
-
-            //playerObjct.MakeTurn(null);
-
+            //    .GetConstructor(new Type[] { typeof(Color) });
+            var ctor = player.GetConstructors().FirstOrDefault();
+            var playerObject = ctor
+                .Invoke(new object[] { color }) as IPlayer;
+            if (playerObject == null)
+                throw new Exception("lol");
+            //Console.WriteLine(playerObject.MakeTurn(new Game().CreateMap()).Count);
+            
+            //var moves = playerObject.MakeTurn( (Checker[,])serializer.Deserialize(Console.ReadLine(), typeof(Checker[,])) );
+            //Console.WriteLine(serializer.Serialize(moves));
 
             //
             // 1. Поднимаете IPlayer (одного) с помощью рефлексии
--- Checkers.Tournament/Program.cs
+++ Checkers.Tournament/Program.cs
@@ -15,25 +15,27 @@ namespace Checkers.Tournament
         Process process;
         JavaScriptSerializer serializer = new JavaScriptSerializer();
 
-        public MyRemotePlayer(string dllName)
+        public MyRemotePlayer(string dllName, Color color)
         {
             //поднимаете два Checkers.Runner вот так:
+            Color = color;
             process = new Process();
             process.StartInfo.FileName = "Checkers.Runner.exe"; //в референсы его!
-            process.StartInfo.Arguments = dllName;
+            process.StartInfo.Arguments = dllName + " " + color.ToString();
             process.StartInfo.UseShellExecute = false; //а может true
             process.StartInfo.RedirectStandardInput = true;
             process.StartInfo.RedirectStandardOutput = true;
-
             process.StartInfo.CreateNoWindow = false; // nado true, just for test bljad
             process.Start();
+
         }
 
 
 
         public Color Color
         {
-            get { throw new NotImplementedException(); }
+            get;
+            private set;
         }
 
         public List<Move> MakeTurn(Checker[,] field)
@@ -49,7 +51,27 @@ namespace Checkers.Tournament
     {
         static void Main(string[] args)
         {
-
+            var white = new MyRemotePlayer(args[0], Color.White);
+            var black = new MyRemotePlayer(args[1], Color.Black);
+            var validator = new Validator();
+            var field = new Checker[8, 8];
+            for (var i = 0; i < 4; i++)
+                for (var j = 0; j < 3; j++)
+                    if (j == 1)
+                    {
+                        field[i * 2 + 1, 7 - j] = new Checker(Color.White, false);
+                        field[i * 2, j] = new Checker(Color.Black, false);
+                    }
+                    else
+                    {
+                        field[i * 2, 7 - j] = new Checker(Color.White, false);
+                        field[i * 2 + 1, j] = new Checker(Color.Black, false);
+                    }
+            while (true)
+            {
+                validator.IsCorrectMove(white.MakeTurn(field), field, Color.White);
+                validator.IsCorrectMove(black.MakeTurn(field), field, Color.Black);
+            }
             // JavascriptSerializer - преобразует че угодно в JSON 
 
             //и м.б. отрисовка?
