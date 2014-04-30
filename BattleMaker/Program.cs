99d63a9d2c8756c1911b601f41f24adc79b11c24
IvanSorokin <iwan954@mail.ru>
2014-04-30 13:11:33 +0600

    fixes

--- BattleMaker/Program.cs
+++ BattleMaker/Program.cs
@@ -30,8 +30,7 @@ namespace Checkers
                 temp = first;
                 foreach (var second in dlls)
                 {
-                    if (temp != second && !used.Contains(new Tuple<string,string>(temp, second))
-                        && !used.Contains(new Tuple<string,string>(second, temp)))
+                    if (temp != second )
                     {
                         result.Add(new Tuple<string, string>(temp, second));
                         used.Add(new Tuple<string,string>(temp, second));
@@ -44,24 +43,28 @@ namespace Checkers
 
     }
 
-    class Program
+    class Program1
     {
         static void Main(string[] args)
         {
-
+            var gamesCount = 9;
             var challenge = new ChallengeObjects();
             foreach (var e in challenge.fighters)
             {
                 var process = new Process();
-                process.StartInfo.FileName = "Checkers.Runner.exe";
+                process.StartInfo.FileName = "Checkers.Tournament.exe";
                 process.StartInfo.Arguments = e.Item1 + " " + e.Item2;
                 process.StartInfo.UseShellExecute = false;
                 process.StartInfo.RedirectStandardInput = true;
                 process.StartInfo.RedirectStandardOutput = true;
-                process.StartInfo.CreateNoWindow = false;
+                process.StartInfo.CreateNoWindow = true;
                 process.Start();
-                //прописать из стандартного аутпута
+                //дописать тле -
+                //рантаймы дописать -
+                //по памяти тоже поставить барьерчик -
+                //прописать из стандартного аутпута -
                 Console.WriteLine(e.Item1 + " " + e.Item2);
+                Console.WriteLine(process.StandardOutput.ReadLine());
             }
             Console.ReadKey();
         }
